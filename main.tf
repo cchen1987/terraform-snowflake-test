f2bdecbb8f40c40fb935712d542075ef1852a963
cchen1987 <chen.11.4.87@gmail.com>
2022-05-27 00:44:52 +0200

    test 2

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "snowflake_role" "role" {
-  provider = snowflake.security_admin
+  provider = snowflake.securityadmin
   name    = var.role_name
 }
 
 resource "snowflake_database" "database" {
-  provider = snowflake.sys_admin
+  provider = snowflake.sysadmin
   name                        = var.database_name
   data_retention_time_in_days = 3
 }
--- provider.tf
+++ provider.tf
@@ -4,9 +4,9 @@ terraform {
       source = "Snowflake-Labs/snowflake"
       version = "0.34.0"
       confconfiguration_aliases = [ 
-        sys_admin,
-        security_admin,
-        account_admin
+        snowflake.sysadmin,
+        snowflake.securityadmin,
+        snowflake.accountadmin
        ]
     }
   }
